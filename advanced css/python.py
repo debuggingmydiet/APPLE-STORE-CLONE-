a60e579c1c1402765cb6fbb0f2ac0912591377a3
Aditya Bainsla <adityabainsla14@gmail.com>
2026-01-02 12:31:08 +0530

    initial commit

---
+++ advanced css/python.py
@@ -0,0 +1,28 @@
+# class Rectangle :
+#     length = None
+#     breadth = None 
+
+#     def __init__(self , l , b ):
+#         self.length = l
+#         self.breadth = b 
+
+#     def perimeter(self):
+#         perimeter = 2*(self.length + self.breadth)
+#         return perimeter 
+        
+
+
+
+#create a class banka ccount with the private balance function 
+
+
+
+# Account number 
+# balance 
+
+
+# funcs-
+# deposit widthdrawl and cheque balance 
+
+
+# widthdrawls should display if insufficient balance 
